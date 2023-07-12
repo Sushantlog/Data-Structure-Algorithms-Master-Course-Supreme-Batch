3447809a3b20c6f1d76a6139a8e25025e86c88bd
Sushantlog <sushantbpatil53@gmail.com>
2023-07-12 13:31:15 +0530

    first commit

---
+++ Basic Programming/For_Loop.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    // ============= for Loop =======================
+    // for(int i=0; i<5; i++){
+    //     cout<<"Sushant Balu Patil "<<endl;
+    // } 
+
+    // =========================================
+
+    // int n;
+    // cout<<"Enter any number :"<<endl;
+    // cin>>n;
+
+    // for(int i=0; i<n; i++){
+    //     cout<<i<<endl;
+    // }
+    
+
+    // ========================================
+    // for(int i=5; i>=0 ; i--){
+    //     cout<<i<<endl;
+    // }
+
+    // for(int i=0; i<10; i=i+2){
+    //     cout<<i<<endl;
+    // }
+
+    // for (int i=1; i<10; i=i*2)  //don't add i=0 
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    // for(int i=100; i>0; i=i/2){
+    //     cout<<i<<endl;
+    // }
+    
+
+    // for(int i=5; i>=0 && i<=10; i=i+1)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    // ================= Home Work =============================
+    // int n;
+    // if(cin>>n){
+    //     cout<<"Sushant "<<endl;
+    // }
+
+    // int m;
+    // if(cout<<m){
+    //     cout<<"Sushant "<<endl;
+    // }
+}
---
+++ Basic Programming/if.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //================== if Condition =====================
+
+    // int score;
+    // cout<<"Enter the Score "<<endl;
+    // cin>>score;
+
+    // if(score<300){
+    //     cout<<"india Win "<<endl;
+    // }
+    // cout<<"Japan win";
+
+    //================ if-else ==============================
+
+    // int age;
+
+    // cout<<"Enter age : ";
+    // cin>>age;
+
+    // if(age>=10){
+    //     cout<<"You can not vote ";
+    // }
+    // else{
+    //     cout<<"You can vote ";
+    // }
+
+    // ============== if-elseif-else ==========================
+    
+    // int grade;
+    // cout<<"Enter Grade : ";
+    // cin>>grade;
+
+    // if(grade >=90){
+    //     cout<<"A Grade "<<endl;
+    // }
+    // else if(grade >= 80){
+    //     cout<<"B Grade "<<endl;
+    // }
+    // else if (grade >= 70)
+    // {
+    //     cout<<"C Grade "<<endl;
+    // }
+    // else if(grade >= 60){
+    //     cout<<"D Grade "<<endl;
+    // }
+    // else{
+    //     cout<<"E Grade "<<endl;
+    // }
+
+    // =================== Example =========================
+
+    // int BroNum;
+    // cout<<"Enter Bro Number ";
+    // cin>>BroNum;
+
+    // if(BroNum == 0){
+    //     cout<<"Baat bangayegi";
+    // }
+    // else{
+    //     cout<<"Baat Nahi Bangayegi";
+    // }
+
+
+
+} 
