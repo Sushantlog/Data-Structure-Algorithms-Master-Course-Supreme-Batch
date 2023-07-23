5112059264c517a1a936cf00029d3a82da837000
Sushantlog <sushantbpatil53@gmail.com>
2023-07-23 23:10:16 +0530

    first commit

---
+++ 1.Basic Programming/Equilty_Operator.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[100];
+    int n=6;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    cout<<"with 10 : "<<endl;
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" "<<endl;;
+    }
+    cout<<"without 10 : "<<endl;
+    for(int i=0; i<n; i++){
+        if(arr[i]!=10){       //print except 10
+            cout<<arr[i]<<" "<<endl;
+        }
+    }
+}
+
+// Output = 
+// 10
+// 52
+// 450
+// 10
+// 255
+// 65
+// with 10 : 
+// 10 
+// 52 
+// 450 
+// 10 
+// 255 
+// 65 
+// without 10 : 
+// 52 
+// 450 
+// 255 
+// 65 
+
+// ================================================
+
