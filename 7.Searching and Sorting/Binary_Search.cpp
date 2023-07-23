55d405e08d8366bf2ef53332ba9925a5c94a1839
Sushantlog <sushantbpatil53@gmail.com>
2023-07-24 00:15:27 +0530

    first commit

---
+++ 7.Searching and Sorting/Binary_Search.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int Binary_Search(int arr[], int n, int Target_Key){
+    int start = 0;
+    int end = n-1;
+    int mid = (start+(end-start)/2);
+
+    while(start <= end){
+        if(arr[mid]==Target_Key){
+            return mid;
+        }
+        else if(arr[mid]>Target_Key){
+            end = mid -1;
+        }
+        else if(arr[mid]<Target_Key){
+            start = mid + 1;
+        }
+        mid = (start+(end-start)/2);
+    }
+
+}
+
+int main(){
+    int arr[10]={1,3,4,6,7,9};
+    int n = 6;
+
+    int Target_key = 7;
+    int location = Binary_Search(arr,n,Target_key);
+
+    cout<<"Target value is present at : "<<location<<" Location"<<endl;
+
+}
+
+// Output = Target value is present at : 4 Location
