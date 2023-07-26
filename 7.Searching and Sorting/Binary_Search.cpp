ef2182d1621e9e616f7dee2727744718c31aea61
Sushantlog <sushantbpatil53@gmail.com>
2023-07-27 01:36:08 +0530

    binary search

--- 7.Searching and Sorting/Binary_Search.cpp
+++ 7.Searching and Sorting/Binary_Search.cpp
@@ -289,3 +289,192 @@ using namespace std;
 
 // ===============================================================
 
+// find Pivot element 
+
+int FindPivot(vector<int>arr){
+    int start = 0;
+    int end = arr.size();
+    int mid = (start+(end-start)/2);
+
+    while(start<=end){
+        if(start == end ){
+            return start;
+        }
+        else if(mid <= end && arr[mid]>arr[mid+1]){
+            return mid; 
+        }
+        else if(mid -1 >= start && arr[mid - 1]>arr[mid]){
+            return mid -1;
+        }
+        else if(arr[start]>arr[mid]){
+            end = mid -1;
+        }
+        else if(arr[start]<arr[mid]){
+            start = mid +1;
+        }
+        mid = (start+(end-start)/2);
+    }
+}
+
+int main(){
+    // vector<int>arr{1,2,10,3,4,5};
+    // vector<int>arr{1,2,10,3,4};
+    // vector<int>arr{1,2,3,4,5,7};
+    vector<int>arr{1,2,10,2,4,5};
+
+    int Pivot = FindPivot(arr);
+
+    cout<<"Pivot element is find at location : "<<Pivot<<endl;
+
+    return 0;
+}
+
+// Output =
+// Pivot element is find at location : 2
+// Pivot element is find at location : 2
+// Pivot element is find at location : 5
+// Pivot element is find at location : 2
+
+// =================================================
+
+
+class Solution {
+public:
+    int findPivot(vector<int>arr){
+    int start = 0;
+    int end = arr.size()-1;
+    int mid = (start+(end-start)/2);
+
+    while(start<=end){
+        if(start == end ){
+            return start;
+        }
+        else if(mid <= end && arr[mid]>arr[mid+1]){
+            return mid; 
+        }
+        else if(mid -1 >= start && arr[mid - 1]>arr[mid]){
+            return mid -1;
+        }
+        else if(arr[start]>arr[mid]){
+            end = mid -1;
+        }
+        else if(arr[start]<arr[mid]){
+            start = mid +1;
+        }
+        mid = (start+(end-start)/2);
+    }
+    return -1;
+}
+    int binary_Search(vector<int>arr,int start, int end, int target){
+        int mid = (start+(end-start)/2);
+
+        while(start <= end){
+        if(arr[mid]==target){
+            return mid;
+        }
+        else if(arr[mid]>target){
+            end = mid -1;
+        }
+        else if(arr[mid]<target){
+            start = mid + 1;
+        }
+        mid = (start+(end-start)/2);
+    }
+    return -1;
+}
+
+    int search(vector<int>& nums, int target) {
+        int pivotIndex = findPivot(nums);
+
+        int ans = 0;
+        if(target >= nums[0] && target <= nums[pivotIndex]){
+            ans = binary_Search(nums, 0, pivotIndex, target);
+        }
+        else{
+            ans = binary_Search(nums, pivotIndex+1,nums.size()-1,target);
+        }
+        return ans;
+    }
+};
+
+
+// ========================================================
+
+// .solve
+
+class Solution {
+public:
+int findPivot(vector<int> arr) {
+  int s = 0;
+  int e = arr.size() - 1;
+  int mid = s + (e-s)/2;
+
+  while(s <= e) {
+    if(s == e) {
+      //single element
+      return s;
+    }
+    if(mid <= e && arr[mid] > arr[mid+1])
+      return mid;
+    if(mid-1 >= s && arr[mid-1] > arr[mid])
+      return mid-1;
+
+    if(arr[s] > arr[mid]) 
+      e = mid - 1;
+    else
+      s = mid + 1;
+
+    mid = s + (e-s)/2;
+
+  }
+  return -1;
+}
+int binarySearch(vector<int> arr, int s, int e, int target) {
+    int mid = s + (e-s)/2;
+
+    while(s <= e) {
+        if(arr[mid] == target) {
+            return mid;
+        }
+        if(arr[mid] > target)
+            e = mid - 1;
+        else
+            s = mid + 1;
+        mid = s +(e-s)/2;
+    }
+    return -1;
+
+
+}
+    int search(vector<int>& nums, int target) {
+
+        int pivotIndex = findPivot(nums);
+        int ans = 0;
+        if(target >= nums[0] && target <= nums[pivotIndex]) {
+            ans = binarySearch(nums, 0, pivotIndex, target);
+
+        }
+        else {
+            ans = binarySearch(nums, pivotIndex+1, nums.size()-1, target);
+        }
+
+    return ans;
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+};
+
+// ==================================================================
+
+
+
