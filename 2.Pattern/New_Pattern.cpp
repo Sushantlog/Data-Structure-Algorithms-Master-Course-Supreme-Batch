caee38aea1d0cbb451f0ae3333e167dd39566cb2
Sushantlog <sushantbpatil53@gmail.com>
2024-06-05 00:03:07 +0530

    pattern solve first commit

---
+++ 2.Pattern/New_Pattern.cpp
@@ -0,0 +1,120 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n; j++){
+//             cout<<" * ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// ---------------------------------
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=0; i<n; i++){
+//         if(i==0 || i==n-1){
+//             for(int i=0; i<n; i++){
+//                 cout<<" * ";
+//             }
+//         }else{
+//             cout<<" * ";
+//             for(int i=0; i<n-2; i++){
+//                 cout<<"   ";
+//             }
+//             cout<<" * ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// ======================================
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<i+1; j++){
+//             cout<<" * ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// =======================================
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n-i; j++){
+//             cout<<" * ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// ==================================
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<i+1; j++){
+//             cout<<j+1;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// =======================================
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n-i; j++){
+//             cout<<j+1;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// =======================
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=0; i<n; i++){
+//             if(i==0 || i==n-1){
+//                 for(int j=0; j<n-i; j++){
+//                     cout<<" * ";
+//                 }
+//             }else{
+//                 cout<<" * ";
+//                 for(int j=0; j<n-i-2; j++){
+//                     cout<<"   ";
+//                 }
+//                 cout<<" * ";
+//             }
+//         cout<<endl;
+//     }
+// }
+
+// Output = 
+//  *  *  *  *  *  *  * 
+//  *              *    
+//  *           *       
+//  *        * 
+//  *     * 
+//  *  * 
+//  * 
+
+// ===============================================
+
+
